2e3bef14fb08b67cde2ab6b6526d5aa3c9f09317
Nathannicolle <nathan-nicolle@orange.fr>
2023-02-14 18:58:17 +0100

    trying to put rs and email into form fields
    for edit part

--- offresEmploi/src/main/kotlin/fr/caensup/offresemploi/controllers/EntrepriseController.kt
+++ offresEmploi/src/main/kotlin/fr/caensup/offresemploi/controllers/EntrepriseController.kt
@@ -2,11 +2,13 @@ package fr.caensup.offresemploi.controllers
 
 import fr.caensup.offresemploi.entities.Entreprise
 import fr.caensup.offresemploi.repositories.EntrepriseRepository
+import jakarta.websocket.server.PathParam
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.view.RedirectView
@@ -35,5 +37,12 @@ class EntrepriseController {
     fun newSubmitAction(@ModelAttribute entreprise: Entreprise) : RedirectView {
         entrepriseRepository.save(entreprise)
         return RedirectView("/entreprise");
+    }
+
+    @GetMapping("/edit/{id}")
+    fun editEntreprise(@PathVariable id : Int, model : ModelMap) : String
+    {
+        model["specificEntreprise"] = entrepriseRepository.findById(id);
+        return "/entreprise/edit";
     }
 }
